857068bc82fa587ad99d0da798c3d5b7b9e48893
toommttom <tom@fluzin.fr>
2025-03-10 12:04:01 +0100

    Création de la branche backend-only

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-// import { createRouter, createWebHistory } from "vue-router";
-// import Home from "./views/HomePage.vue";
-import router from "./router/router.js";
-
-// Créer l'application Vue et lier le routeur
-createApp(App).use(router).mount("#app");
