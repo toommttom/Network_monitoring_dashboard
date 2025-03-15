44bb4a6917ddcf7865d6541db0d4d338aa03d5bb
toommttom <tom@fluzin.fr>
2025-03-15 17:03:19 +0100

    changement pour fast api

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
-from flask import Flask
-from flask_cors import CORS
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.app.routes import router  # Import des routes FastAPI
 
-app = Flask(__name__)
-CORS(app)  # Permet la communication avec le frontend
+# Création de l'application FastAPI
+app = FastAPI()
 
-from app import routes
+# Autoriser les requêtes du frontend (CORS)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Autorise toutes les origines (à sécuriser en prod)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Inclure les routes définies dans routes.py
+app.include_router(router)
